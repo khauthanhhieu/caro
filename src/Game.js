9da07266182a03a0518d8dec9c1f7dba510125b8
khauthanhhieu <thanhhieu021298@gmail.com>
2019-09-29 21:12:39 +0700

    to màu ô kia thắng

--- src/Game.js
+++ src/Game.js
@@ -31,140 +31,48 @@ class Board extends React.Component {
         />;
     }
 
-    isWin = function (table, x, y) {
-        var red = this.state.red.slice
+    isWinBy(table, x, y, vector) {
+        var red = this.state.red
+        var XO = table[y * 20 + x]
         for (let i = 0; i < 5; i++) {
-            let lengthX = 0, lengthY = 0, lengthXY = 0, lengthYX = 0
-
-            for (let j = i; j > i - 5; j--) {
-                if (x + j >= 0 && x + j < 20) {
-                    switch (table[y * 20 + x + j]) {
-                        case "X":
-                            lengthX++;
-                            break;
-                        case "O":
-                            lengthX--;
-                            break;
-                        default:
-                    }
-                    switch (table[(y + j) * 20 + x]) {
-                        case "X":
-                            lengthY++;
-                            break;
-                        case "O":
-                            lengthY--;
-                            break;
-                        default:
-                    }
-                    switch (table[(y + j) * 20 + x + j]) {
-                        case "X":
-                            lengthXY++;
-                            break;
-                        case "O":
-                            lengthXY--;
-                            break;
-                        default:
-                    }
-                    switch (table[(y - j) * 20 + x + j]) {
-                        case "X":
-                            lengthYX++;
-                            break;
-                        case "O":
-                            lengthYX--;
-                            break;
-                        default:
-                    }
+            let sx = x - i * vector[0]
+            let sy = y - i * vector[1]
+            let flat = true
+            for (let j = 0; j < 5; j++) {
+                let X = sx + j * vector[0]
+                let Y = sy + j * vector[1]
+                if (X >= 20 || Y >= 20 || X < 0 || Y < 0 || table[Y * 20 + X] !== XO) {
+                    flat = false
+                    break
                 }
             }
-            // ngang 1
-            if (lengthX === 5 && (table[y * 20 + x + i - 5] !== "O" || table[y * 20 + x + i + 1] !== "O")) {
-                red[y * 20 + x + i - 4] = true
-                red[y * 20 + x + i - 3] = true
-                red[y * 20 + x + i - 2] = true
-                red[y * 20 + x + i - 1] = true
-                red[y * 20 + x + i] = true
-                this.setState({
-                    red: red
-                })
-                return "X"
+            if (flat === true) {
+                var OX = null
+                if (XO === "O") {
+                    OX = "X"
+                } else if (XO === "X") {
+                    OX = "O"
+                }
+                if (table[(sy - vector[1]) * 20 + sx - vector[0]] !== OX || table[(sy + 5 * vector[1]) * 20 + sx + 5 * vector[0]] !== OX) {
+                    for (let j = 0; j < 5; j++) {
+                        let X = sx + j * vector[0]
+                        let Y = sy + j * vector[1]
+                        red[Y * 20 + X] = true
+                    }
+                    return XO
+                }
             }
-            //ngang 2
-            if (lengthX === -5 && (table[y * 20 + x + i - 5] !== "X" || table[y * 20 + x + i + 1] !== "X")) {
-                red[y * 20 + x + i - 4] = true
-                red[y * 20 + x + i - 3] = true
-                red[y * 20 + x + i - 2] = true
-                red[y * 20 + x + i - 1] = true
-                red[y * 20 + x + i] = true
-                this.setState({
-                    red: red
-                })
-                return "O"
-            }
-            if (lengthY === 5 && (table[(y + i - 5) * 20 + x] !== "O" || table[(y + i + 1) * 20 + x] !== "O")) {
-                red[(y + i - 4) * 20 + x] = true
-                red[(y + i - 3) * 20 + x] = true
-                red[(y + i - 2) * 20 + x] = true
-                red[(y + i - 1) * 20 + x] = true
-                red[(y + i) * 20 + x] = true
-                this.setState({
-                    red: red
-                })
-                return "X"
-            }
-            if (lengthY === -5 && (table[(y + i - 5) * 20 + x] !== "X" || table[(y + i + 1) * 20 + x] !== "X")) {
-                red[(y + i - 5) * 20 + x + i - 5] = true
-                red[(y + i + 1) * 20 + x + i + 1] = true
-                this.setState({
-                    red: red
-                })
-                return "O"
-            }
-            if (lengthXY === 5 && (table[(y + i - 5) * 20 + x + i - 5] !== "O" || table[(y + i + 1) * 20 + x + i + 1] !== "O")) {
-                red[(y + i - 4) * 20 + x + i - 4] = true
-                red[(y + i - 3) * 20 + x + i - 3] = true
-                red[(y + i - 2) * 20 + x + i - 2] = true
-                red[(y + i - 1) * 20 + x + i - 1] = true
-                red[(y + i) * 20 + x + i] = true
-                this.setState({
-                    red: red
-                })
-                return "X"
-            }
-            if (lengthXY === -5 && (table[(y + i - 5) * 20 + x + i - 5] !== "X" || table[(y + i + 1) * 20 + x + i + 1] !== "X")) {
-                red[(y + i - 4) * 20 + x + i - 4] = true
-                red[(y + i - 3) * 20 + x + i - 3] = true
-                red[(y + i - 2) * 20 + x + i - 2] = true
-                red[(y + i - 1) * 20 + x + i - 1] = true
-                red[(y + i) * 20 + x + i] = true
-                this.setState({
-                    red: red
-                })
-                return "O"
-            }
-            if (lengthYX === 5 && (table[(y - i + 5) * 20 + x + i - 5] !== "O" || table[(y - i - 1) * 20 + x + i + 1] !== "O")) {
-                red[(y - i + 4) * 20 + x + i - 4] = true
-                red[(y - i + 3) * 20 + x + i - 3] = true
-                red[(y - i + 2) * 20 + x + i - 2] = true
-                red[(y - i + 1) * 20 + x + i - 1] = true
-                red[(y - i) * 20 + x + i] = true
-                
-                this.setState({
-                    red: red
-                })
-                return "X"
-            }
-            if (lengthYX === -5 && (table[(y - i + 5) * 20 + x + i - 5] !== "X" || table[(y - i - 1) * 20 + x + i + 1] !== "X")) {
-                red[(y - i + 4) * 20 + x + i - 4] = true
-                red[(y - i + 3) * 20 + x + i - 3] = true
-                red[(y - i + 2) * 20 + x + i - 2] = true
-                red[(y - i + 1) * 20 + x + i - 1] = true
-                red[(y - i) * 20 + x + i] = true
+        }
+        return null
+    }
 
-                this.setState({
-                    red: red
-                })
-                return "O"
-            }
+    isWin(table, x, y) {
+        var vector = [[1, 0], [0, 1], [1, 1], [1, -1]]
+
+        for (let i = 0; i < 4; i++) {
+            var XO = this.isWinBy(table, x, y, vector[i])
+            if (XO)
+                return XO
         }
         return null
     }
